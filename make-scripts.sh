686d2c5a573cb8951c4bc79d6723c8d1845e6bb4
Scott B <28817345+foundObjects@users.noreply.github.com>
2020-01-05 14:36:56 -0800

    version 3 first draft; more reliable hook trigger removal, installer now looks for a copy of pve-nag-buster.sh in the same directory before fetching/emitting a copy

--- make-scripts.sh
+++ make-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_VERS="v02"
+_VERS="v03"
 
 _OUTFILE="pve-nag-buster.sh"
 
@@ -10,4 +10,4 @@ _OUTFILE="install.sh"
 
 sed "s/__VERSION__/$_VERS/g" src/install > $_OUTFILE
 xz -z -9 -c pve-nag-buster.sh | base64 >> $_OUTFILE
-echo "YEET" >> $_OUTFILE
+printf "ENDMALWARE\n}\n" >> $_OUTFILE
